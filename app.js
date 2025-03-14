222148ddb541b232a3c56f4cafb72a6f729c6f4f
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-03-14 16:41:49 +0300

    تحديث الكود

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.sendFile('./views/home.html',{root: __dirname})
 })
-app.get('/', (req, res) => {
-    res.sendFile('./views/home.html',{root: __dirname})
-  })
+
   
 app.listen(port, () => {
   console.log(`http://localhost:${port}/ `)

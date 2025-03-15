f3390233dd88384757ff32c270c8dd6767aa4e47
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-03-15 14:16:00 +0300

    تحديث الدرس الرابع

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose= require('mongoose');
+app.use(express.urlencoded({ extended: true }));
+const Mydatamodel = require('./models/mydataCHEMA');
+ 
+
 
 app.get('/', (req, res) => {
   res.sendFile('./views/home.html',{root: __dirname})
@@ -20,4 +24,22 @@ mongoose
 })
 .catch((err)=>{
   console.log(err);
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  
+  const mydat= new Mydatamodel(req,res)
+  mydat.save()
+  .then(()=>{
+    res.sendFile('./views/sucsses.html',{root: __dirname})
+  })
+  .catch((err)=>{
+    console.log(err);  
+  })
+  
+
+  
+
+
 })
